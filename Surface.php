81590ead22752f34371f18f2ce7a9b6062a76f42
celia77940 <celiarameau@live.fr>
2020-10-26 16:19:16 +0100

    surfaceformulaire

--- Surface.php
+++ Surface.php
@@ -1,10 +1,21 @@
 <?php
 /*Calcule de surface d'un rectangle*/
-$largeur = 20;
-$longueur = 20;
+$largeur = $_POST['largeur'];
+$longueur = $_POST['longueur'];
 
 $surface = $largeur * $longueur;
 
 echo $surface;
 
 ?>
+
+<form  action = "" method="POST" >
+<label for="longueur">longueur</label>
+<input type ="number" name="longueur"></input>;
+<br> </br>
+<label for="largeur">largeur</label>
+<input type ="number" name="largeur"></input>;
+<br> </br>
+<input type ="submit" value ="calcul"></input>
+
+</form>
